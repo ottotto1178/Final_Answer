2839f0e38c1410f7ce4e883c4e290dfb387c8a1e
ottotto1178 <128988854+ottotto1178@users.noreply.github.com>
2023-04-05 06:39:22 +0900

    Delete Exercise_for_Pool/python directory

--- Exercise_for_Pool/python/test.py
+++
@@ -1,9 +0,0 @@
-/html/body/div[1]/div[2]/div[4]/div[1]/section[8]/div/div/ul/li/a
-/html/body/div[1]/div[2]/div[4]/div[1]/section[7]/div/div/ul/li/a
-
-
-//*[@id="sv-site"]/li/a
-//*[@id="sv-site"]/li/a
-//*[@id="sv-site"]/li/a
-//*[@id="sv-site"]/li/a
-//*[@id="sv-site"]/li/a
