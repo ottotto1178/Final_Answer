a0fab818d84b122e9f952a2b54b024c77aec84c9
ottotto1178 <128988854+ottotto1178@users.noreply.github.com>
2023-04-05 04:28:31 +0900

    Add files via upload

---
+++ Exercise_for_Pool/python/test.py
@@ -0,0 +1,9 @@
+/html/body/div[1]/div[2]/div[4]/div[1]/section[8]/div/div/ul/li/a
+/html/body/div[1]/div[2]/div[4]/div[1]/section[7]/div/div/ul/li/a
+
+
+//*[@id="sv-site"]/li/a
+//*[@id="sv-site"]/li/a
+//*[@id="sv-site"]/li/a
+//*[@id="sv-site"]/li/a
+//*[@id="sv-site"]/li/a
